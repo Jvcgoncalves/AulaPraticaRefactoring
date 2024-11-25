d2e558f6eb2c28b461e7c6486f1bf1f1c0bb5256
goncalves <goncalvesdacosta05@gmail.com>
2024-11-25 17:01:23 -0300

    Commit 3

--- Main.java
+++ Main.java
@@ -16,25 +16,15 @@ public class Main {
 
   private static void testMovie() {
     Main._movie = new Movie("Filmão", 1);
-    System.out.println("Movie title: "
-        .concat(Main._movie.getTitle())
-        .concat(" - Code for get value: ")
-        + Main._movie.getPriceCode());
-    // Main._movie.setPriceCode(2);
   }
 
   private static void testRental() {
     Main._rental = new Rental(Main._movie, 7);
-    System.out
-        .println("Movie: " + Main._rental.getMovie().getTitle() + " - Days rented: " + Main._rental.getDaysRented());
   }
 
   private static void testCustomer() {
     Movie movie = new Movie("Filminho", 0);
     Rental rental = new Rental(movie, 10);
-
-    System.out
-        .println("Movie: " + rental.getMovie().getTitle() + " - Days rented: " + rental.getDaysRented());
 
     Main._customer = new Customer("Jão");
 
