47400b4217c3ea9d37f1feb25d9f015fb4af6c34
goncalves <goncalvesdacosta05@gmail.com>
2024-11-26 20:43:56 -0300

    Commit 18

--- classes/statement/Statement.java
+++ classes/statement/Statement.java
@@ -1,10 +1,21 @@
 package classes.statement;
 
+import java.util.Enumeration;
 import classes.customer.Customer;
 import classes.rental.Rental;
 
 public abstract class Statement {
-  public abstract String value(Customer aCustomer);
+
+  public String value(Customer aCustomer) {
+    StringBuilder result = new StringBuilder(header(aCustomer));
+    Enumeration rentals = aCustomer.getRentals();
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      result.append(eachRentalString(each));
+    }
+    result.append(footer(aCustomer));
+    return result.toString();
+  }
 
   protected abstract String header(Customer aCustomer);
 
--- classes/statement/textStatement/TextStatement.java
+++ classes/statement/textStatement/TextStatement.java
@@ -1,22 +1,10 @@
 package classes.statement.textStatement;
 
-import java.util.Enumeration;
 import classes.customer.Customer;
 import classes.rental.Rental;
 import classes.statement.Statement;
 
 public class TextStatement extends Statement {
-
-  public String value(Customer aCustomer) {
-    StringBuilder result = new StringBuilder(header(aCustomer));
-    Enumeration rentals = aCustomer.getRentals();
-    while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
-      result.append(eachRentalString(each));
-    }
-    result.append(footer(aCustomer));
-    return result.toString();
-  }
 
   protected String header(Customer aCustomer) {
     return "Rental Record for " + aCustomer.getName() + "\n";
