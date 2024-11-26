6ac466b76eb78e4774a4657228159ea1b6f313ac
goncalves <goncalvesdacosta05@gmail.com>
2024-11-26 20:37:58 -0300

    Commit 16

---
+++ classes/statement/HtmlStatement/HtmlStatement.java
@@ -0,0 +1,28 @@
+package classes.statement.HtmlStatement;
+
+import java.util.Enumeration;
+
+import classes.customer.Customer;
+import classes.rental.Rental;
+import classes.statement.Statement;
+
+public class HtmlStatement extends Statement {
+  public String value(Customer aCustomer) {
+    Enumeration rentals = aCustomer.getRentals();
+    String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
+        "</EM></H1><P>\n";
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      // show figures for each rental
+      result += each.getMovie().getTitle() + ": " +
+          String.valueOf(each.getCharge()) + "<BR>\n";
+    }
+    // add footer lines
+    result += "<P>You owe <EM>" +
+        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+    result += "On this rental you earned <EM>" +
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        "</EM> frequent renter points<P>";
+    return result;
+  }
+}
---
+++ classes/statement/Statement.java
@@ -0,0 +1,5 @@
+package classes.statement;
+
+public class Statement {
+
+}
